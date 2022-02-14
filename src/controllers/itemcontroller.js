4c769d1625261006ba0adb7f720144ad53cf8c0f
szabinakov <szabinkov@gmail.com>
2022-02-14 11:02:21 +0000

    added logic for removing an item from the basket, using items from data.js

--- src/controllers/itemcontroller.js
+++ src/controllers/itemcontroller.js
@@ -1,3 +1,4 @@
+let items = require("../../data")
 
 let basket = []
 
@@ -6,6 +7,21 @@ exports.addNewItem = async(req,res) => {
         const newItem = req.body
         basket.push(newItem)
         res.status(200).json(basket)
+    }catch(error){
+        res.status(500).json({ error: "Internal Server Error"})
+    }
+}
+
+exports.removeItemById = async(req,res) => {
+    const id = req.params.itemId
+
+    try{
+        // filters through items already in basket
+        // function deletes item where id matches the id sent in request parameter and send back new array, please NOTE this logic sends back the array untouched when there was nothing to remove!!!
+        const newBasket = items.filter(element => {
+            return element.id !== parseInt(id)
+        })
+        res.status(200).json(newBasket)
     }catch(error){
         res.status(500).json({ error: "Internal Server Error"})
     }
