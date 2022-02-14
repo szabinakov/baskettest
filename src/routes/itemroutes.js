5fa90126b438f596415a28894a74d8100b6a091c
szabinakov <szabinkov@gmail.com>
2022-02-14 10:46:10 +0000

    required the item controller file in routes

--- src/routes/itemroutes.js
+++ src/routes/itemroutes.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const router = express.Router();
+const itemController = require("../controllers/itemcontroller")
 
 router.post("/", itemController.addNewItem)
 
