c3d9fb5a750358f655d528bd0e33ce7e8e3e4556
szabinakov <szabinkov@gmail.com>
2022-02-14 11:51:05 +0000

    created route for listing all items

--- src/routes/itemroutes.js
+++ src/routes/itemroutes.js
@@ -2,10 +2,13 @@ const express = require("express")
 const router = express.Router();
 const itemController = require("../controllers/itemcontroller")
 
+router.get("/listAllItems", itemController.listAllItems)
+
 router.post("/", itemController.addNewItem)
 
 router.delete("/:itemId", itemController.removeItemById)
 
 router.delete("/", itemController.emptyBasket)
+
 
 module.exports = router
