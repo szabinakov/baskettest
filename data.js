ecf8608a30a342f53573d85d195ddd7e68e3bd4a
szabinakov <szabinkov@gmail.com>
2022-02-14 10:34:05 +0000

    created a data.js file with some data for demonstration used for testing as well

---
+++ data.js
@@ -0,0 +1,46 @@
+let items = [
+    {
+        title: "Item1",
+        price: 100,
+        weight: 1,
+        deliveryDay: 4,
+        id : 1
+    },
+    {
+        title: "Item2",
+        price: 200,
+        weight: 8,
+        deliveryDay: 1,
+        id : 2
+    },
+    {
+        title: "Item3",
+        price: 300,
+        weight: 7,
+        deliveryDay: 2,
+        id : 3
+    },
+    {
+        title: "Item4",
+        price: 400,
+        weight: 4,
+        deliveryDay: 10,
+        id : 4
+    },
+    {
+        title: "Item5",
+        price: 500,
+        weight: 3,
+        deliveryDay: 3,
+        id : 5
+    },
+    {
+        title: "Item6",
+        price: 500,
+        weight: 2,
+        deliveryDay: 5,
+        id : 6
+    }
+]
+
+module.exports = items
