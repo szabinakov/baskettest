97454b281181f7c8543426ebbf9986ba0fe3681f
szabinakov <szabinkov@gmail.com>
2022-02-14 10:46:57 +0000

    finished logic for adding a new item, controller function

--- src/controllers/itemcontroller.js
+++ src/controllers/itemcontroller.js
@@ -3,7 +3,9 @@ let basket = []
 
 exports.addNewItem = async(req,res) => {
     try{
-
+        const newItem = req.body
+        basket.push(newItem)
+        res.status(200).json(basket)
     }catch(error){
         res.status(500).json({ error: "Internal Server Error"})
     }
